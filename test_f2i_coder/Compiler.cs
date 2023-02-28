a2f967fa633069d6896f9c86f390ae63b0a23c8a
FIEF-nohell <noel.hmnn@gmail.com>
2023-02-28 08:49:55 +0100

    compiler WIP

--- test_f2i_coder/Compiler.cs
+++ test_f2i_coder/Compiler.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,43 @@ namespace f2i_coder
         public Compiler()
         {
             InitializeComponent();
+        }
+
+        private void Compiler_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                    {
+                        using (BinaryReader binaryReader = new BinaryReader(fileStream))
+                        {
+                            byte[] binaryData = binaryReader.ReadBytes((int)fileStream.Length);
+                            Console.WriteLine(BitConverter.ToString(binaryData));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            stopwatch1.Stop();
+            TimeSpan elapsed1 = stopwatch1.Elapsed;
         }
     }
 }
