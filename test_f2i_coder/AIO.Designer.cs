cafc4f100d84fb2fb93b5a477f2727a74c4cd37d
FIEF-nohell <noel.hmnn@gmail.com>
2023-03-02 17:40:01 +0100

    icon added

--- test_f2i_coder/AIO.Designer.cs
+++ test_f2i_coder/AIO.Designer.cs
@@ -238,6 +238,7 @@
             this.Controls.Add(this.c_select_file);
             this.Controls.Add(this.decompiler_title);
             this.Controls.Add(this.compiler_title);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1019, 420);
             this.MinimumSize = new System.Drawing.Size(1019, 420);
